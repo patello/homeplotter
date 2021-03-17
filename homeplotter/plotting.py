608accb4831b4e4cdf70220305617a3cff31bab4
Patello <patrk.ekenberg@gmail.com>
2021-03-17 06:48:59 +0000

    Moved summary into a file instead of printing

--- homeplotter/plotting.py
+++ homeplotter/plotting.py
@@ -19,37 +19,42 @@ categories.append("All")
 tags = summed_account.get_tags()
 tags.remove("Reservation")
 
-print("\n------Tag Summary (Month)------\n")
+summary_texts = []
+summary_texts.append("------Tag Summary (Month)------")
+tag_average = []
 for tag in tags:
     summed_account.reset_filter()
     summed_account.filter_data("tags","==",tag)
-    summed_account.filter_data("date",">=",datetime.date(2020,12,15))
-    print("{tag}: {amount}\n".format(tag=tag,amount=math.ceil(summed_account.get_average("Month"))))
-    # for acc_delta in ["Week", "Month"]:
-    #     plt.cla()
-    #     tsdata = summed_account.get_timeseries()
-    #     tsdata.accumulate(1,acc_delta)
-    #     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-    #     if acc_delta == "Week":
-    #         plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
-    #     else:
-    #         plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-    #     plt.plot(tsdata.get_x(),tsdata.get_y())
-    #     plt.gcf().autofmt_xdate()
-    #     plt.savefig('./output/{acc_delta}ly/{tag}.png'.format(tag=tag,acc_delta=acc_delta.lower()))
+    summed_account.filter_data("date",">=",datetime.date(2020,12,1))
+    tag_average.append([tag,math.ceil(summed_account.get_average("Month"))])
+    if False:
+        for acc_delta in ["Week", "Month"]:
+            plt.cla()
+            tsdata = summed_account.get_timeseries()
+            tsdata.accumulate(1,acc_delta)
+            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+            if acc_delta == "Week":
+                plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
+            else:
+                plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+            plt.plot(tsdata.get_x(),tsdata.get_y())
+            plt.gcf().autofmt_xdate()
+            plt.savefig('./output/{acc_delta}ly/tags/{tag}.png'.format(tag=tag,acc_delta=acc_delta.lower()))
+for tag in sorted(tag_average,key=lambda tag:-tag[1]):
+    summary_texts.append("{tag}: {amount}".format(tag=tag[0],amount=tag[1]))
 
-
-print("\n------Category Summary (Month)------\n")
+cat_average = []
+summary_texts.append("\n------Category Summary (Month)------")
 for category in categories:
     summed_account.reset_filter()
 
-    summed_account.filter_data("date",">=",datetime.date(2020,12,15))
+    summed_account.filter_data("date",">=",datetime.date(2020,12,1))
     if category != "All":
         summed_account.filter_data("category","==",category)
     else:
-        #Try to remove transfers from all to get a bit more consistency
+        #Remove transfers from all to get a bit more consistency
         summed_account.filter_data("category","!=","Transfer")
-    print("{category}: {amount}\n".format(category=category,amount=math.ceil(summed_account.get_average("Month"))))
+    cat_average.append([category,math.ceil(summed_account.get_average("Month"))])
     summed_account.filter_data("tags","!=","Reservation")
     for acc_delta in ["Week", "Month"]:
         plt.cla()
@@ -63,9 +68,16 @@ for category in categories:
         plt.plot(tsdata.get_x(),tsdata.get_y())
         plt.gcf().autofmt_xdate()
         plt.savefig('./output/{acc_delta}ly/{category}.png'.format(category=category,acc_delta=acc_delta.lower()))
+for cat in sorted(cat_average,key=lambda cat:-cat[1]):
+    summary_texts.append("{cat}: {amount}".format(cat=cat[0],amount=cat[1]))
 
 with open("./output/categories.csv", "w") as f:
     summed_account.reset_filter()
-    for data in summed_account.get_data():
+    for data in reversed(summed_account.get_data()):
         f.write(", ".join([data[3],data[2],str(data[4])]))
+        f.write("\n")
+
+with open("./output/summary.txt", "w") as f:
+    for row in summary_texts:
+        f.write(row)
         f.write("\n")
