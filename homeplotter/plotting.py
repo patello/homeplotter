6f3de37a68041426235507ca6e41558a1bcc5308
Patello <patrk.ekenberg@gmail.com>
2021-06-07 18:51:53 +0000

    Removed weekly plotting, too noisy

--- homeplotter/plotting.py
+++ homeplotter/plotting.py
@@ -9,7 +9,8 @@ cat_file="./data/personal_categories.json"
 tag_file="./data/personal_tags.json"
 account_data1 = AccountData("./data/konto_gemensamt.csv",cat_file,tag_file)
 account_data2 = AccountData("./data/konto_personligt.csv",cat_file,tag_file)
-summed_account = account_data1/2 + account_data2
+account_data3 = AccountData("./data/konto_ica.csv",cat_file,tag_file)
+summed_account = account_data1/2 + account_data2 + account_data3 / 3
 
 categories = summed_account.get_categories()
 categories.remove("Uncategorized")
@@ -25,21 +26,20 @@ tag_average = []
 for tag in tags:
     summed_account.reset_filter()
     summed_account.filter_data("tags","==",tag)
-    summed_account.filter_data("date",">=",datetime.date(2020,12,1))
+    summed_account.filter_data("date",">=",datetime.date(2021,2,1))
     tag_average.append([tag,math.ceil(summed_account.get_average("Month"))])
-    if False:
-        for acc_delta in ["Week", "Month"]:
-            plt.cla()
-            tsdata = summed_account.get_timeseries()
-            tsdata.accumulate(1,acc_delta)
-            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-            if acc_delta == "Week":
-                plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
-            else:
-                plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-            plt.plot(tsdata.get_x(),tsdata.get_y())
-            plt.gcf().autofmt_xdate()
-            plt.savefig('./output/{acc_delta}ly/tags/{tag}.png'.format(tag=tag,acc_delta=acc_delta.lower()))
+    acc_delta = "Month"
+    plt.cla()
+    tsdata = summed_account.get_timeseries()
+    tsdata.accumulate(1,acc_delta)
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    if acc_delta == "Week":
+        plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
+    else:
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+    plt.plot(tsdata.get_x(),tsdata.get_y())
+    plt.gcf().autofmt_xdate()
+    plt.savefig('./output/tags/{tag}.png'.format(tag=tag,acc_delta=acc_delta.lower()))
 for tag in sorted(tag_average,key=lambda tag:-tag[1]):
     summary_texts.append("{tag}: {amount}".format(tag=tag[0],amount=tag[1]))
 
@@ -56,18 +56,18 @@ for category in categories:
         summed_account.filter_data("category","!=","Transfer")
     cat_average.append([category,math.ceil(summed_account.get_average("Month"))])
     summed_account.filter_data("tags","!=","Reservation")
-    for acc_delta in ["Week", "Month"]:
-        plt.cla()
-        tsdata = summed_account.get_timeseries()
-        tsdata.accumulate(1,acc_delta)
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
-        if acc_delta == "Week":
-            plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
-        else:
-            plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
-        plt.plot(tsdata.get_x(),tsdata.get_y())
-        plt.gcf().autofmt_xdate()
-        plt.savefig('./output/{acc_delta}ly/{category}.png'.format(category=category,acc_delta=acc_delta.lower()))
+    acc_delta = "Month"
+    plt.cla()
+    tsdata = summed_account.get_timeseries()
+    tsdata.accumulate(1,acc_delta)
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+    if acc_delta == "Week":
+        plt.gca().xaxis.set_major_locator(mdates.WeekdayLocator())
+    else:
+        plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+    plt.plot(tsdata.get_x(),tsdata.get_y())
+    plt.gcf().autofmt_xdate()
+    plt.savefig('./output/{category}.png'.format(category=category,acc_delta=acc_delta.lower()))
 for cat in sorted(cat_average,key=lambda cat:-cat[1]):
     summary_texts.append("{cat}: {amount}".format(cat=cat[0],amount=cat[1]))
 
