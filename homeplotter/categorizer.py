1aab143b81df77461d3c499bbab8cece93cf3cba
Patello <patrk.ekenberg@gmail.com>
2021-07-06 19:08:30 +0000

    Made it possible to append tags to categorizer

--- homeplotter/categorizer.py
+++ homeplotter/categorizer.py
@@ -1,6 +1,7 @@
 import json
 import re
 import collections
+from typing import List
 
 class Categorizer():
     def __init__(self, cfile, mode="categorize"):
@@ -13,6 +14,7 @@ class Categorizer():
             #Using ordered dict since I need to makes sure it's the last key that matches everything.
             #Seems like we can use an ordinary dict from python 3.7 and on. 
             self._rec_cat = collections.OrderedDict(data)
+            self._tag_parents = {}
             if self._mode=="categorize":
                 self._rec_cat["Uncategorized"] = [""]
             if self._mode=="tag":
@@ -52,8 +54,20 @@ class Categorizer():
                                     else:
                                         levels[ret_level]=ret_levels[ret_level]
                     return levels
-                #Tag levels need to be extracted from _rec_cat before tag_unnesting as it flattens the levels
-                self._tag_levels=levels_unnesting(self._rec_cat,0)
+                def parent_unnesting(tag_list,parent=None):
+                    parents = {}
+                    for tag in tag_list:
+                        if tag != "":
+                            if parent is not None:
+                                parents[tag]=parent
+                            if type(tag_list[tag]) is dict:
+                                ret_parents = parent_unnesting(tag_list[tag],tag)
+                                for ret_parent in ret_parents:
+                                    parents[ret_parent] = ret_parents[ret_parent]
+                    return parents
+
+                #Tag parents need to be extracted from _rec_cat before tag_unnesting as it flattens the levels
+                self._tag_parents=parent_unnesting(self._rec_cat)
                 (_,self._rec_cat)=tag_unnesting(self._rec_cat)
         
     def match(self,reciever):
@@ -82,10 +96,47 @@ class Categorizer():
         return dict.keys(self._rec_cat)
     
     def get_levels(self):
-        if self._mode=="tag":
-            return self._tag_levels
+        tag_levels = {}
+        for tag in self._rec_cat:
+            level = self.get_level(tag)
+            if level in tag_levels:
+                tag_levels[level].append(tag)
+            else:
+                tag_levels[level]=[tag]
+        return tag_levels
+
+    def get_level(self,tag):
+        level = 0
+        next_tag = tag
+        while next_tag in self._tag_parents:
+            next_tag = self._tag_parents[next_tag]
+            level+=1
+        return level
+
+    def append(self,tag,text,parent_tag=None):
+        #For the purposes of this function, a tag can also be a category if mode ="Category"
+        #Check if tag already exists.
+        if tag in self._rec_cat:
+            #If it exists, append the text to the list or dict
+            if type(self._rec_cat[tag])==list:
+                self._rec_cat[tag].append(text)
+            else:
+                #If it is a dictionary, it belongs to a tag that has children, but isn't part of the children.
+                #They are denoted with an empty string.
+                self._rec_cat[tag][""].append(text)
+            #If the tag has parents, it needs to be added recurively to them as well. 
+            if tag in self._tag_parents:
+                self.append(self._tag_parents[tag],text)
         else:
-            raise(NotImplemented)
+            #Else, create a new tag with the text.
+            self._rec_cat[tag]=[text]
+            if parent_tag is not None:
+                self._tag_parents[tag]=parent_tag
+                #Recursively add the tag to the parents as well
+                self.append(parent_tag,text)
+            if self._mode=="categorize":
+                #If mode is categorize, the "Uncategorized function need to be moved to the end of the dict
+                self._rec_cat.move_to_end("Uncategorized")
 
 if __name__=="__main__":
     categorizer = Categorizer("/root/projects/homeplotter/example_data/categories.json")
--- tests/test_categorizer.py
+++ tests/test_categorizer.py
@@ -29,12 +29,49 @@ def test_match_tag_nested():
     assert(tagger.match("C3")==["tagABC"])
     assert(tagger.match("E2")==["tag2"])
 
-def test_tag_levels():
+def test_tag_get_levels():
     tagger = Categorizer(tag_nested_path,mode="tag")
-    assert(list(tagger.get_levels().keys())==[0,1,2])
+    assert(list(tagger.get_levels().keys())==[1,2,0])
     assert(tagger.get_levels()[0]==["tagABC","tag2"])
     assert(tagger.get_levels()[1]==["A","B"])
     assert(tagger.get_levels()[2]==["B1","B23"])
+
+def test_tag_get_level():
+    tagger = Categorizer(tag_nested_path,mode="tag")
+    assert(tagger.get_level("tagABC")==0)
+    assert(tagger.get_level("B23")==2)
+
+def test_category_append():
+    categorizer = Categorizer(cat_path)
+    categorizer.append("cat1","Lorem")
+    assert(categorizer.match("Lorem ipsum")=="cat1")
+    categorizer.append("new cat","sit")
+    assert(categorizer.match("dolor sit amet")=="new cat")
+
+def test_tag_append_flat():
+    tagger = Categorizer(tag_path,mode="tag")
+    tagger.append("tag1","Lorem")
+    assert(tagger.match("Lorem ipsum")==["tag1"])
+    tagger.append("new tag","A1")
+    assert(tagger.match("dolor sit amet A1")==["tag1","new tag"])
+
+def test_tag_append_nested():
+    tagger = Categorizer(tag_nested_path,mode="tag")
+    tagger.append("tagABC","Lorem")
+    assert(tagger.match("Lorem ipsum")==["tagABC"])
+    tagger.append("A","sit")
+    assert(tagger.match("dolor sit amet")==["A","tagABC"])
+    tagger.append("new tag","elit","B23")
+    assert(tagger.match("consectetur adipiscing elit")==["B23","B","tagABC","new tag"])
+    tagger.append("new top tag","sed")
+    assert(tagger.match("sed do")==["new top tag"])
+
+def test_tag_append_nested_levels():
+    tagger = Categorizer(tag_nested_path,mode="tag")
+    tagger.append("new tag","elit","B23")
+    assert(tagger.get_levels()[3]==["new tag"])
+    tagger.append("new top tag","sed")
+    assert(tagger.get_levels()[0]==["tagABC","tag2","new top tag"])
 
 if __name__=="__main__":
     test_match_category()
