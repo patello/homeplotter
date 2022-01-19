d14e6f9f7ed40636afbb33bcc7b86bcce14c7993
Patello <patrk.ekenberg@gmail.com>
2022-01-19 19:21:54 +0000

    Updated tests

--- tests/test_accountdata_update.py
+++ tests/test_accountdata_update.py
@@ -21,22 +21,39 @@ def test_update():
 
     #Old data
     summed_account.filter_data("date","==",datetime.date(2021, 1, 1))
-    assert(summed_account.get_column("text")==["Lorem Ipsum"])
+    assert(summed_account.get_column("text")==["Lorem Ipsum","Lorem Ipsum"])
     summed_account.reset_filter()
 
     #New data
     summed_account.filter_data("date","==",datetime.date(2021, 1, 5))
-    assert(summed_account.get_column("text")==["New data","A2"])
+    assert(summed_account.get_column("text")==["A1","SWISH FRÅN Namn"])
     summed_account.reset_filter()
 
     #Overlapping data should be updated
-    summed_account.filter_data("date","==",datetime.date(2021, 1, 5))
-    assert(summed_account.get_column("text")==["Updated data"])
-    assert(summed_account.get_column("amount")==[333.33/2])
+    summed_account.filter_data("date","==",datetime.date(2021, 1, 4))
+    assert(summed_account.get_column("text")==['201229 A1', '201230 A3 ', '210101 B1', '210102 a4', 'Updated data'])
+    #Last one is the new data (not reliable so change if it starts failing)
+    assert(summed_account.get_column("amount")[-1]==[333.33/2])
     summed_account.reset_filter()
 
+@pytest.mark.skip(reason="Update not implemented")
+def test_update__daterange():
+    account_data1 = AccountData("./example_data/data1.csv",tag_file="./example_data/tags_nested.json")
+    account_data2 = AccountData("./example_data/data2.csv",account_name="other_data")
+
+    summed_account = account_data1 + account_data2/2
+    old_range = summed_account._daterange
+
+    summed_account.update("./example_data/data_new.csv", "data2")
+    new_range = summed_account._daterange
+
+    #Start date is same
+    assert(old_range[0]==new_range[0])
+    #New date range is longer however
+    assert(old_range[1]<new_range[1])
+
 @pytest.mark.skip(reason="Save/load not implemented")
-def test_update_save_load():
+def test_update__save_load():
     account_data1 = AccountData("./example_data/data1.csv",tag_file="./example_data/tags_nested.json")
     account_data2 = AccountData("./example_data/data2.csv",account_name="other_data")
 
